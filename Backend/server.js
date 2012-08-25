1e47426d9ce29c754bd82ec5aa21801f71249529
Malte Rohde <malte.rohde@inf.fu-berlin.de>
2012-08-25 23:43:52 +0200

    add passport, basic authentication

--- Backend/server.js
+++ Backend/server.js
@@ -1,20 +1,40 @@
 var application_root = __dirname,
 	express = require("express"),
-	path = require("path");
+	path = require("path"),
+	passport = require("passport"),
+	LocalStrategy = require('passport-local').Strategy;
 var app = express();
 
-// Config
+// Config.
 app.configure(function () {
+	app.use(express.cookieParser());
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
+	app.use(passport.initialize());
 	app.use(app.router);
 	app.use(express.static(path.join(application_root, "../Frontend")));
 	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-app.get('/api', function (req, res) {
-	res.send('API is running');
+// Passport config.
+passport.use(new LocalStrategy(
+	function(username, password, done) {
+		console.log(username);
+		console.log(password);
+		done(null, "test");
+	})
+);
+
+// API.
+app.get('/loginfailed', function (req, res) {
+	res.send("Login failed.");
 });
+
+app.get('/login', 
+	passport.authenticate('local', { failureRedirect: '/loginfailed' }),
+	function (req, res) {
+		res.redirect('/loginfailed');	
+	});
 
 // Launch server
 app.listen(4242);
