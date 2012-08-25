27de50317424a87a807825cc3d3b8d467c936f6a
Malte Rohde <malte.rohde@inf.fu-berlin.de>
2012-08-25 23:13:32 +0200

    server.js: Set ../Frontend as static files directory.

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ app.configure(function () {
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(app.router);
-	app.use(express.static(path.join(application_root, "public")));
+	app.use(express.static(path.join(application_root, "../Frontend")));
 	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
