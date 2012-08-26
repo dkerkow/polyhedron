e117e3afe95a557fcef17471aa3bacbcfa4ac197
Malte Rohde <malte.rohde@inf.fu-berlin.de>
2012-08-26 02:04:00 +0200

    server.js: Passport work.

--- Backend/server.js
+++ Backend/server.js
@@ -1,44 +1,73 @@
 var application_root = __dirname,
+	userdb = require("userdb");
 	express = require("express"),
 	path = require("path"),
 	passport = require("passport"),
 	LocalStrategy = require('passport-local').Strategy;
 var app = express();
+var UserDb = userdb.UserDb;
+var SessionDb = userdb.SessionDb;
 
 // Config.
 app.configure(function () {
-	app.use(express.cookieParser());
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
+	app.use(express.cookieParser("SOMESECRET"));
+	app.use(express.session({
+		secret: "SOMESECRET",
+      	}));
 	app.use(passport.initialize());
+	app.use(passport.session());	
 	app.use(app.router);
 	app.use(express.static(path.join(application_root, "../Frontend")));
 	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
+
 // Passport config.
 passport.use(new LocalStrategy(
-	function(username, password, done) {
-		console.log(username);
-		console.log(password);
-		done("Kann ich noch nicht.", false);
+	function(username, password, done) {	
+		var user = UserDb.lookup(username, hash);
+		if(!user)
+			done("User not found.", null);
+		else
+			done(null, user);
 	})
 );
+
+passport.serializeUser(function(user, done) {
+	// TODO: Create random session id.
+	var sid = (new Date()).getTime();
+	SessionDb.store(sid, user.id);
+	done(null, sid);
+});
+
+passport.deserializeUser(function(sid, done) {
+	var user = SessionDb.lookup(sid);
+	if(!user)
+		done("Session not found.", null);
+	else
+		done(null, user);
+});
 
 // API.
 app.get('/loginfailed', function (req, res) {
 	res.send("Login failed.");
 });
 
-
-app.get('/loginsucceeded', function (req, res) {
-	res.send("Login succeeded.");
-});
-
 app.get('/login', 
 	passport.authenticate('local', { failureRedirect: '/loginfailed' }),
 	function (req, res) {
-		res.redirect('/loginsucceeded');	
+		res.redirect('/home');	
+	});
+
+app.get('/home',
+	function (req, res) {
+		console.log(req.user);
+		if(!req.user)
+			res.send("Not authenticated.");
+		else		
+			res.send("Home.");	
 	});
 
 // Launch server
